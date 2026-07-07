dd05da4344948d94c9efcf8a89769d57263410f6
kija mnada <tanprograma@gmail.com>
2026-07-07 09:11:22 +0300

    fixed page rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'sales',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'reports',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'detailed-reports',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'purchasing',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'inventory',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
